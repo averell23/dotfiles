da3b5c212051d753dd2c9440259f400c9f87ca20
Daniel Hahn <dha@betterplace.org>
2013-11-05 10:40:39 +0100

    Remove android kit, should be installed through system packages (brew)

--- adk/adk.zsh
+++
@@ -1,6 +0,0 @@
-if test -d /Developer/adt
-then
-  export PATH=$PATH:/Developer/adt/sdk/tools
-  export PATH=$PATH:/Developer/adt/sdk/platform-tools
-  export PATH=$PATH:/Developer/adt/sdk/build-tools
-fi
