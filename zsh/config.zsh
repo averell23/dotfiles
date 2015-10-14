40547099e2a9e2407e35bd136aabe7615a43b82c
Daniel Hahn <dha@betterplace.org>
2015-10-14 17:09:17 +0200

    Move to oh-my-zsh and fancyness
    * oh-my-zsh and zgen
    * fzf fuzzy autocomplete
    * autosuggest disabled for now, incompatible w/ fzf
    * theme from omz used

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,17 +1,7 @@
-if [[ -n $SSH_CONNECTION ]]; then
-  export PS1='%m:%3~$(git_info_for_prompt)%# '
-else
-  export PS1='%3~$(git_info_for_prompt)%# '
-fi
-
 export GATEWAY_USER=dhahn
 export EDITOR=vim
 export LSCOLORS="exfxcxdxbxegedabagacad"
 export CLICOLOR=true
-
-fpath=($ZSH/zsh/functions $fpath)
-
-autoload -U $ZSH/zsh/functions/*(:t)
 
 HISTFILE=~/.zsh_history
 HISTSIZE=10000
