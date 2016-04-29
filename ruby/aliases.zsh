4cd833b5bf0211dbf96d60041c372f86bf8c7b1e
Daniel Hahn <dha@betterplace.org>
2016-04-29 10:51:54 +0200

    Rails console alias

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,6 +1,7 @@
 alias bx="bundle exec"
 alias spek="spring rspec"
 alias rakk="spring rake"
+alias rc="bundle exec rails c"
 alias cdb="cd /Developer/betterplace"
 alias cdc="cd /Developer/provisioning/chef-repo"
 alias cda="cd /Developer/provisioning/betterplace-provisioning"
