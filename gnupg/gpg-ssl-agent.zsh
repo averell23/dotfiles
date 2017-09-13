ae072eb6119345f51d622063c6a603932f4fc4b8
Daniel Hahn <dha@betterplace.org>
2017-09-13 16:28:26 +0200

    Fix gpg agent config for some machines

--- gnupg/gpg-ssl-agent.zsh
+++ gnupg/gpg-ssl-agent.zsh
@@ -1,2 +1,4 @@
-gpg-connect-agent /bye
+# Enforce use of the gpg2 agent, instead of others that may be around...
+/usr/local/opt/gpg2/bin/gpg-connect-agent /bye
 export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+. $HOME/.gpg-agent-info
