aff455587db8e298362abe191ca5105b909a5338
Daniel Hahn <dha@betterplace.org>
2017-12-18 17:17:06 +0100

    Add gnupg

--- gnupg/gpg-ssl-agent.zsh
+++ gnupg/gpg-ssl-agent.zsh
@@ -1,4 +1,3 @@
-# Enforce use of the gpg2 agent, instead of others that may be around...
-/usr/local/opt/gpg2/bin/gpg-connect-agent /bye
+gpg-connect-agent /bye
 export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
 . $HOME/.gpg-agent-info
