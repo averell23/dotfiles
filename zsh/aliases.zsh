1e162a3c48e27172c62c8fd4a4030e5296baa7fb
Daniel Hahn <dha@betterplace.org>
2018-02-09 17:46:18 +0100

    Alias and scripting update
    * Adjust aliases
    * Enable dotfiles bin folder
    * make "upall" a script

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,8 +1,5 @@
 alias reload!='. ~/.zshrc'
-alias upall='git pull && bundle && bin/yarn && rake db:migrate'
 alias cdd='cd ~/Desktop'
-alias privdrop='encfs ~/Dropbox/private.encfs ~/private_box'
-alias bpkeys='encfs ~/Google\ Drive/access_keys.encfs ~/betterplace_keys'
 bps() { ssh betterplace@bp-$1.betterops.de }
 epo() { ssh betterplace@epo-$1.betterops.de }
 dha-bps() { ssh daniel.hahn@bp-$1.betterops.de }
