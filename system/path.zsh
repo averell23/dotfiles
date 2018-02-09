3611fbd4e1ee560ee8a0520e9886e912e1501fd9
Daniel Hahn <dha@betterplace.org>
2018-02-09 17:46:18 +0100

    Alias and scripting update
    * Adjust aliases
    * Enable dotfiles bin folder
    * make "upall" a script

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,2 @@
-export PATH="/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
-
+export PATH="/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH:$DOTFILES/bin"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
