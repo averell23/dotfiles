260671c2caf18462bf3b2f6577b0b2c94f52fb7e
Daniel Hahn <dha@betterplace.org>
2018-09-20 17:54:37 +0200

    Don't use gpg-agent any more.

--- gnupg/gpg-ssl-agent.zsh
+++ gnupg/gpg-ssl-agent.zsh
@@ -1,3 +1,3 @@
-gpg-connect-agent /bye
-export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+# gpg-connect-agent /bye
+# export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
 # . $HOME/.gpg-agent-info
