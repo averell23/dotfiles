66b0be1b4b1c0f0d9aba11a28536ae1d7a3ede4e
Daniel Hahn <dha@betterplace.org>
2019-02-09 09:45:33 +0100

    Some adjustments

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,6 @@
 alias reload!='. ~/.zshrc'
 alias cdd='cd ~/Desktop'
-alias cdt='cd ~/Code/averell23/travelsite'
+alias cdt='cd ~/Code/averell23/theurbananimals'
 alias upall='git pp && script/update'
 bps() { ssh betterplace@bp-$1.betterops.de }
 epo() { ssh betterplace@epo-$1.betterops.de }
