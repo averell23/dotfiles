005b8c909abe9133926c68eca182d4e77a88ef14
Daniel Hahn <dha@betterplace.org>
2019-06-25 12:33:38 +0200

    Add more macOS defaults

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -8,7 +8,9 @@
 # Remember you can read current values with defaults read ...
 #
 # There are more involved things like
-# https://github.com/mathiasbynens/dotfiles/blob/master/.macos
+#
+# * https://github.com/mathiasbynens/dotfiles/blob/master/.macos
+# * https://www.defaults-write.com/
 #
 # but I prefer not to mess with defaults that are generally sane
 #
@@ -113,6 +115,70 @@ defaults write -g ApplePressAndHoldEnabled -bool false
 # Use AirDrop over every interface. srsly this should be a default.
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 
+# Disable Dashboard
+defaults write com.apple.dashboard mcx-disabled -bool true
+
+##########################################################################
+# Dock and Spaces
+
+# Don’t animate opening applications from the Dock
+# defaults write com.apple.dock launchanim -bool false
+
+# Speed up Mission Control animations
+# defaults write com.apple.dock expose-animation-duration -float 0.1
+
+# Don’t group windows by application in Mission Control
+# (i.e. use the old Exposé behavior instead)
+# defaults write com.apple.dock expose-group-by-app -bool false
+
+# Enable highlight hover effect for the grid view of a stack (Dock)
+defaults write com.apple.dock mouse-over-hilite-stack -bool true
+
+# Set the icon size of Dock items to 64 pixels
+# defaults write com.apple.dock tilesize -int 64
+
+# Change minimize/maximize window effec (genie or scale)
+defaults write com.apple.dock mineffect -string "genie"
+
+# Minimize windows into their application’s icon
+# defaults write com.apple.dock minimize-to-application -bool true
+
+# Enable spring loading for all Dock items
+# defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true
+
+# Show indicator lights for open applications in the Dock
+# defaults write com.apple.dock show-process-indicators -bool true
+
+# Wipe all (default) app icons from the Dock
+# This is only really useful when setting up a new Mac, or if you don’t use
+# the Dock to launch apps.
+#defaults write com.apple.dock persistent-apps -array
+
+
+# Disable Dashboard
+defaults write com.apple.dashboard mcx-disabled -bool true
+
+# Don’t show Dashboard as a Space
+defaults write com.apple.dock dashboard-in-overlay -bool true
+
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+
+# Remove the auto-hiding Dock delay
+# defaults write com.apple.dock autohide-delay -float 0
+
+# Remove the animation when hiding/showing the Dock
+# defaults write com.apple.dock autohide-time-modifier -float 0
+
+# Make Dock icons of hidden applications translucent
+defaults write com.apple.dock showhidden -bool true
+
+# Show recent applications in Dock
+defaults write com.apple.dock show-recents -bool true
+
+# Disable the Launchpad gesture (pinch with thumb and three fingers)
+#defaults write com.apple.dock showLaunchpadGestureEnabled -int 0
+
 # Set a really fast key repeat.
 # defaults write NSGlobalDomain KeyRepeat -int 1
 
@@ -188,3 +254,27 @@ defaults write com.apple.commerce AutoUpdate -bool true
 
 # Allow the App Store to reboot machine on macOS updates
 defaults write com.apple.commerce AutoUpdateRestartRequired -bool true
+
+
+##########################################################################
+# Photos
+
+# Prevent Photos from opening automatically when devices are plugged in
+defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
+
+echo "macOS defaults written"
+
+###############################################################################
+# Kill affected applications
+
+for app in "Activity Monitor" \
+	"cfprefsd" \
+	"Dock" \
+	"Finder" \
+	"Mail" \
+	"Photos" \
+	"Safari" \
+	"SystemUIServer" \
+; do
+	killall "${app}" &> /dev/null
+done
