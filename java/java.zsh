b223709090edb51643c959ea17ed4c91486eda2c
Daniel Hahn <dha@betterplace.org>
2021-02-16 17:09:00 +0100

    rm java stuff (use asdf instead)

--- java/java.zsh
+++
@@ -1,9 +0,0 @@
-if test -f /usr/libexec/java_home
-then
-  export JAVA_HOME=`/usr/libexec/java_home`
-fi
-if test -d "$HOME/.jenv/bin"
-then
-  export PATH="$HOME/.jenv/bin:$PATH"
-  eval "$(jenv init -)"
-fi
