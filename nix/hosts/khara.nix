e8da3949a05d1da9a75c975f5fa94d038c5dca8c
Daniel Hahn <daniel.hahn@betterplace.org>
2026-05-04 14:23:51 +0200

    Fixes for khara

--- nix/hosts/khara.nix
+++ nix/hosts/khara.nix
@@ -3,7 +3,7 @@
   homebrew.taps = [ "macos-fuse-t/cask" ];
     homebrew.casks = [
         "blackhole-16ch" # virtual audio driver for WSJT-X audio routing
-        "eddie"
+        "cryptomator"
         "fuse-t"
         "lm-studio"
         "slack"
